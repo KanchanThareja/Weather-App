67a974871b2fcf74dc3bb6825687d0328853508c
Kanchan Thareja <kthareja12@gmail.com>
2019-01-11 07:14:15 -0800

    errors fixed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import Tiles from './components/tiles';
 class App extends Component {
   state = {
     data: [],
-    prevdate : undefined,
+    date: '',
+    humidity: 0,
+    temperature: 0,
+    pressure: 0,
+    time : ''
   }
   getweather = async (e) => {
     e.preventDefault();
@@ -15,11 +19,17 @@ class App extends Component {
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=73c146e4ee5ae67f94bff6e87db44483&units=metric`);
     const dataW = await api_call.json();
     const dateString = dataW.list[0].dt_txt;
-
+    console.log(dataW);
     this.setState({data : dataW.list});
-    const dt  = dateString.split(/\-|\s/);
+    this.setState({humidity : dataW.list[0].main.humidity});
+    this.setState({pressure : dataW.list[0].main.pressure});
+    this.setState({temperature : dataW.list[0].main.temp});
+
+    const dt = dateString.split(/\-|\s/);
     const date = new Date(dt.slice(0,3).reverse().join('-') + ' ' + dt[3]);
-    console.log(date);
+    this.setState({date: date.toString().substring(0,10)});
+    this.setState({time: dateString.substring(11,20)});
+    console.log(date.toString().substring(0,10));
     console.log(dateString.substring(11,20));
     } else {
     console.log("error: invalid input");
@@ -33,7 +43,7 @@ class App extends Component {
       navigator.geolocation.getCurrentPosition(function(position){
        const lat = position.coords.latitude;
        const lon = position.coords.longitude;
-      const api_c = fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=73c146e4ee5ae67f94bff6e87db44483`);
+       const api_c = fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=73c146e4ee5ae67f94bff6e87db44483`);
       //const d = api_c.json();
       //console.log(d);
       //this.setState({data : dataWI});
@@ -50,7 +60,12 @@ class App extends Component {
         <h1>Open Weather Forecast</h1>
           <Form getweather={this.getweather} /><br />
           <button onClick={this.handleclick}>Use my location</button>
-          <Tiles data={this.state.data} />
+          <Tiles data={this.state.data}
+          date={this.state.date}
+          time={this.state.time}
+          humidity = {this.state.humidity}
+          pressure = {this.state.pressure}
+          temperature = {this.state.temperature} />
         </header>
       </div>
     );
--- src/components/tiles.js
+++ src/components/tiles.js
@@ -1,27 +1,22 @@
 import React,{Component} from 'react';
 
 class Tiles extends React.Component {
+   i = 0;
   state = {
     index : 0,
+    curridx : 0
   }
-  date = () => {
-    const dateString = this.props.data[0].dt_txt;
-    const dt = dateString.split(/\-|\s/);
-    const dat = new Date(dt.slice(0,3).reverse().join('-') + ' ' + dt[3]);
-    return dat;
-  }
+
   render() {
     const {tracks} = this.props.data;
 
       return (
-      <div>
-      {tracks.map((track, k) => {
-    return(
-      <div>
-      </div>
-    );
-  })}
-
+      <div className="tile">
+      <h5 onClick={() => {this.setState({index: 0})}}>{this.props.date}</h5>
+      <h5 onClick={() => {this.setState({index: 0})}}>{this.props.time}</h5>
+      <h5 onClick={() => {this.setState({index: 0})}}>humidity :  {this.props.humidity}</h5>
+      <h5 onClick={() => {this.setState({index: 10})}}>temperature : {this.props.temperature}</h5>
+      <h5 onClick={() => {this.setState({index: 15})}}>pressure : {this.props.pressure}</h5>
       </div>
     );
 }
